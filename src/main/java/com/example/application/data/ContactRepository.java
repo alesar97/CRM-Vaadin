97de4b8a528e3a44ba94302267fe0f1b9898570a
start.vaadin.com <no-reply@vaadin.com>
2024-02-13 14:53:32 +0000

    Generated project

---
+++ src/main/java/com/example/application/data/ContactRepository.java
@@ -0,0 +1,8 @@
+package com.example.application.data;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+}
---
+++ src/main/java/com/example/application/data/StatusRepository.java
@@ -0,0 +1,8 @@
+package com.example.application.data;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatusRepository extends JpaRepository<Status, Long> {
+
+}
